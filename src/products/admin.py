2800c8f1ac8b9e2d370765e56644d311132bd43e
sagar <sagarborse90@gmail.com>
2021-08-28 23:32:32 +0530

    added products to admin

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from products.models import Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Product, ProductAdmin)
